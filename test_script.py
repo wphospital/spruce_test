fb01ee757f6938f1addea5f3ef0822dea2e4fcdb
Sege <jsege@wphospital.org>
2022-02-16 14:46:19 -0500

    comment out db_username override

--- test_script.py
+++ test_script.py
@@ -15,8 +15,8 @@ def main(name, sleep):
         sleepy_time = np.random.normal(loc=0.5, scale=0.3)
         time.sleep(sleepy_time)
 
-    if name == 'Sprucey':
-        name = os.getenv('db_username')
+    # if name == 'Sprucey':
+    #     name = os.getenv('db_username')
 
     print('Hello {}'.format(name))
 
